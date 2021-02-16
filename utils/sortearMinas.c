14ea42915d2e77dce412738a7463731794ccd12c
liaporto <moonkoala23@gmail.com>
2021-02-16 15:00:52 -0300

    criar structs para o tabuleiro e para as dificuldades do jogo

--- utils/sortearMinas.c
+++ utils/sortearMinas.c
@@ -1,14 +1,14 @@
-void sortearMinas(int **tabuleiro, int tamTabuleiro)
+void sortearMinas(int **tabuleiro, DIFICULDADE dificuldade)
 {
   int contador = 0;
-  const int numeroMinas = (tamTabuleiro * tamTabuleiro) / 5;
+  const int numeroMinas = dificuldade.nMinas;
   srand(time(0));
 
   while (contador < numeroMinas)
   {
-    for (int linha = 0; linha < tamTabuleiro && contador < numeroMinas; linha++)
+    for (int linha = 0; linha < dificuldade.tam && contador < numeroMinas; linha++)
     {
-      for (int coluna = 0; coluna < tamTabuleiro; coluna++)
+      for (int coluna = 0; coluna < dificuldade.tam; coluna++)
       {
         const int random = rand() % 11;
 
