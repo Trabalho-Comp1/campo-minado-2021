b248484aac1be30a87cf132836232fd9f3210add
liaporto <moonkoala23@gmail.com>
2021-02-16 18:47:21 -0300

    fix: corrigir sintaxe da inicializaçao das dificuldades

---
+++ defs.h
@@ -0,0 +1,25 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define FACIL 1
+#define MEDIO 2
+#define DIFICIL 3
+#define PLAYING 1
+#define GAME_OVER 2
+#define BOMBA -1
+#define CASA_LIVRE 0
+
+typedef struct 
+{
+  int tam;
+  int nMinas;
+} DIFICULDADE;
+
+typedef struct 
+{
+  DIFICULDADE dificuldade;
+  int **jogavel;
+  int **espelho;
+} TABULEIRO;
+
+#endif
